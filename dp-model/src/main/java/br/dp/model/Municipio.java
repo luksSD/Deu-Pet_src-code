743b9338de7a4306a4fa4d46a5d37cb65106bf9b
Lucas Dias <lucas.sd94@gmail.com>
2022-10-06 18:43:19 -0300

    feat: criado entidade municipio

---
+++ dp-model/src/main/java/br/dp/model/Municipio.java
@@ -0,0 +1,12 @@
+package br.dp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Municipio extends BasePojo {
+
+    private String nome;
+    private String uf;
+}

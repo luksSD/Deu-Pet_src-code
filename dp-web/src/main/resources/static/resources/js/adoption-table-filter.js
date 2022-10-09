3007edeb502ed1ec5ae13440da3a0d02333b8cd1
Lucas Dias <lucas.sd94@gmail.com>
2022-10-09 18:53:11 -0300

    fix: corrigido filtro da tabela de animais e exibido alerta de tabela vazia

--- dp-web/src/main/resources/static/resources/js/adoption-table-filter.js
+++ dp-web/src/main/resources/static/resources/js/adoption-table-filter.js
@@ -1,17 +1,100 @@
 $(document).ready(function() {
+
+    alertEmptyTable();
+
     $("input:checkbox").on("click", function() {
 
-        $("#animal-table tr").hide()
-        var flag = 1
-        $("input:checkbox:checked").each(function() {
+        $("#table-body tr").hide()
+
+        var flag = 1;
+        var cbAnimalArray = [];
+        var cbStatusArray = [];
+        var dogFlag = false;
+        var catFlag = false;
+        var adoptedFlag = false;
+        var adoptFlag = false;
+
+        $("input:checkbox[name=animal]:checked").each(function(){
             flag = 0;
             var value = $(this).val().toLowerCase();
-            $("#myTable tr").filter(function() {
-                if ($(this).text().toLowerCase().indexOf(value) > -1)
-                    $(this).show()
-            });
+
+            if ((value.toLowerCase().indexOf("cachorro") > -1)){
+                dogFlag = true;
+            } else{
+                catFlag = true;
+            }
         });
-        if (flag == 1)
-            $("#myTable tr").show()
+
+        $("input:checkbox[name=status]:checked").each(function(){
+            flag = 0;
+            var value = $(this).val().toLowerCase();
+
+            if (value.toLowerCase().indexOf("adotado") > -1){
+                adoptedFlag = true;
+            } else{
+                adoptFlag = true;
+            }
+        });
+
+        if (flag == 1){
+            $("#table-body tr").show()
+        } else{
+            $("#table-body tr").filter(function() {
+
+                if((dogFlag == true && catFlag == true) || (adoptedFlag == true && adoptFlag == true)){
+                    $(this).show()
+                } else if((dogFlag == true) && ($(this).text().toLowerCase().indexOf("cachorro") > -1)){
+                    if(adoptFlag == true && adoptedFlag == false){
+                       if ($(this).text().toLowerCase().indexOf("para adoção") > -1)
+                          $(this).show()
+                    } else if(adoptFlag == false && adoptedFlag == true){
+                        if ($(this).text().toLowerCase().indexOf("adotado") > -1)
+                           $(this).show()
+                    } else {
+                        $(this).show()
+                    }
+                } else if((catFlag == true) && ($(this).text().toLowerCase().indexOf("gato") > -1)){
+                    if(adoptFlag == true && adoptedFlag == false){
+                       if ($(this).text().toLowerCase().indexOf("para adoção") > -1)
+                          $(this).show()
+                    } else if(adoptFlag == false && adoptedFlag == true){
+                        if ($(this).text().toLowerCase().indexOf("adotado") > -1)
+                           $(this).show()
+                    } else {
+                        $(this).show()
+                    }
+                } else if((adoptedFlag == true) && ($(this).text().toLowerCase().indexOf("adotado") > -1)){
+                    if(dogFlag == true && catFlag == false){
+                       if ($(this).text().toLowerCase().indexOf("cachorro") > -1)
+                          $(this).show()
+                    } else if(dogFlag == false && catFlag == true){
+                        if ($(this).text().toLowerCase().indexOf("gato") > -1)
+                           $(this).show()
+                    } else {
+                        $(this).show()
+                    }
+                } else if((adoptFlag == true) && ($(this).text().toLowerCase().indexOf("para adoção") > -1)){
+                    if(dogFlag == true && catFlag == false){
+                       if ($(this).text().toLowerCase().indexOf("cachorro") > -1)
+                          $(this).show()
+                    } else if(dogFlag == false && catFlag == true){
+                        if ($(this).text().toLowerCase().indexOf("gato") > -1)
+                           $(this).show()
+                    } else {
+                        $(this).show()
+                    }
+                }
+            });
+        }
+        alertEmptyTable();
     });
+
+    function alertEmptyTable(){
+      if($('#custom-table tbody tr:visible').length <= 0){
+          $("#noEntryAlert").show()
+      } else{
+          $("#noEntryAlert").hide()
+      }
+    }
 });
+

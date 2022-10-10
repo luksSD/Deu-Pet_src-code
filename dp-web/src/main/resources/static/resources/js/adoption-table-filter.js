541739822cf046da778e0c4f84ba43d1ef0a9d13
Lucas Dias <lucas.sd94@gmail.com>
2022-10-10 19:40:44 -0300

    fix: corrigido sintaxe

--- dp-web/src/main/resources/static/resources/js/adoption-table-filter.js
+++ dp-web/src/main/resources/static/resources/js/adoption-table-filter.js
@@ -95,7 +95,7 @@ $(document).ready(function() {
             });
         }
         alertEmptyTable();
-    })};
+    });
 
     function alertEmptyTable(){
       if($('#custom-table tbody tr:visible').length <= 0){

636f86ca4164fb5bc600e89599734ce0453be4bd
Lucas Dias <lucas.sd94@gmail.com>
2022-10-16 19:02:28 -0300

    fix: corrigido inputs senha no cadastro de instituicao

--- dp-web/src/main/resources/static/resources/js/show-password.js
+++ dp-web/src/main/resources/static/resources/js/show-password.js
@@ -1,28 +1,28 @@
 function showPassword1() {
-  var passwordInput = document.getElementById("passwordInput");
+  var passwordInput1 = document.getElementById("passwordInput1");
   var showPassword1 = document.getElementById("showPassword1");
 
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
+  if (passwordInput1.type === "password") {
+    passwordInput1.type = "text";
     showPassword1.classList.remove("mdi-eye");
     showPassword1.classList.add("mdi-eye-off");
   } else {
-    passwordInput.type = "password";
+    passwordInput1.type = "password";
     showPassword1.classList.remove("mdi-eye-off");
     showPassword1.classList.add("mdi-eye");
   }
 }
 
 function showPassword2() {
-  var passwordInput = document.getElementById("passwordInput");
+  var passwordInput2 = document.getElementById("passwordInput2");
   var showPassword2 = document.getElementById("showPassword2");
 
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
+  if (passwordInput2.type === "password") {
+    passwordInput2.type = "text";
     showPassword2.classList.remove("mdi-eye");
     showPassword2.classList.add("mdi-eye-off");
   } else {
-    passwordInput.type = "password";
+    passwordInput2.type = "password";
     showPassword2.classList.remove("mdi-eye-off");
     showPassword2.classList.add("mdi-eye");
   }

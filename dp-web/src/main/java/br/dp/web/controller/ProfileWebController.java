cd75757feb2194ec5ea1aaab9d4611bfbd0c76f4
Lucas Dias <lucas.sd94@gmail.com>
2022-11-26 00:47:37 -0300

    feat: implementado alteracao de senha do perfil de admin

--- dp-web/src/main/java/br/dp/web/controller/ProfileWebController.java
+++ dp-web/src/main/java/br/dp/web/controller/ProfileWebController.java
@@ -4,6 +4,7 @@ import br.dp.model.Instituicao;
 import br.dp.model.Usuario;
 import br.dp.web.security.provider.DpAuthenticationProvider;
 import br.dp.web.service.FileService;
+import br.dp.web.service.UserService;
 import br.dp.web.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,16 @@ import java.security.AuthProvider;
 @RequestMapping("/perfil")
 public class ProfileWebController {
 
+    public static String message = "";
+
     @Autowired
     private DpAuthenticationProvider authProvider;
 
     @Autowired
     private FileService fileService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/detalhes")
     public String getProfileDetailPage(Model model) {
@@ -30,6 +36,15 @@ public class ProfileWebController {
         user.setProfileImg(file != null ? file : Constants.USER_DEFAULT_IMG);
         model.addAttribute("usuario", user);
 
+        if (!message.equals("")) {
+            if (message.contains("sucesso")) {
+                model.addAttribute("succesMessage", message);
+            } else {
+                model.addAttribute("errorMessage", message);
+            }
+            message = "";
+        }
+
         return "profile/profile-detail-page";
     }
 
@@ -38,6 +53,22 @@ public class ProfileWebController {
 
         model.addAttribute("user", user);
         return "profile/profile-edit-page";
+    }
+
+    @PostMapping("/alterar-senha")
+    public String changePassword(@RequestParam(value = "password") final String senha, Usuario user) {
+
+        user.setSenha(senha);
+
+        final boolean response = userService.updatePassword(user);
+
+        if (response) {
+            message = "Senha alterada com sucesso!";
+        } else {
+            message = "Não foi possível alterar a senha. Tente novamente!";
+        }
+        return "redirect:/perfil/detalhes/";
+
     }
 
 }
